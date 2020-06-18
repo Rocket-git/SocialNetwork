dd141b2f23040fa8875cb259b3e0a317bb38fc31
Rocket-git <doroshenko.roman.page@gmail.com>
2020-06-18 22:51:16 +0300

    added index.html file

---
+++ precache-manifest.64c32d600497af6dbfd936f2d2c24140.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46aab0867e95d19eeafcb951d750bd31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd00726e47422e279f05",
+    "url": "/static/css/3.e96cc935.chunk.css"
+  },
+  {
+    "revision": "f125ea8de84727a57260",
+    "url": "/static/css/4.61af993a.chunk.css"
+  },
+  {
+    "revision": "f314db2b1e787a3bd64a",
+    "url": "/static/css/main.e98700f0.chunk.css"
+  },
+  {
+    "revision": "e4a60114bc5ab6c4414c",
+    "url": "/static/js/2.4560c424.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/static/js/2.4560c424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd00726e47422e279f05",
+    "url": "/static/js/3.2d086e38.chunk.js"
+  },
+  {
+    "revision": "f125ea8de84727a57260",
+    "url": "/static/js/4.dc5c51d0.chunk.js"
+  },
+  {
+    "revision": "f314db2b1e787a3bd64a",
+    "url": "/static/js/main.cd5941c4.chunk.js"
+  },
+  {
+    "revision": "3b48577393b8f31b71d5",
+    "url": "/static/js/runtime-main.be2ccc80.js"
+  },
+  {
+    "revision": "c863daf947cd38a8aaf09b192dd4c8dc",
+    "url": "/static/media/preloader2.c863daf9.gif"
+  }
+]);
