9b015b1a310295d68f93407f42653208c802e706
Rocket-git <doroshenko.roman.page@gmail.com>
2020-06-20 00:07:38 +0300

    Updates

---
+++ precache-manifest.b781ff34bb44469aed8575e618b58d85.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dc040d3454a22ec80f5b7baea0b82c8",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "bd00726e47422e279f05",
+    "url": "/SocialNetwork/static/css/3.e96cc935.chunk.css"
+  },
+  {
+    "revision": "f125ea8de84727a57260",
+    "url": "/SocialNetwork/static/css/4.61af993a.chunk.css"
+  },
+  {
+    "revision": "48c6d391385d32a1b7b9",
+    "url": "/SocialNetwork/static/css/main.e98700f0.chunk.css"
+  },
+  {
+    "revision": "e4a60114bc5ab6c4414c",
+    "url": "/SocialNetwork/static/js/2.4560c424.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/SocialNetwork/static/js/2.4560c424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd00726e47422e279f05",
+    "url": "/SocialNetwork/static/js/3.2d086e38.chunk.js"
+  },
+  {
+    "revision": "f125ea8de84727a57260",
+    "url": "/SocialNetwork/static/js/4.dc5c51d0.chunk.js"
+  },
+  {
+    "revision": "48c6d391385d32a1b7b9",
+    "url": "/SocialNetwork/static/js/main.1031f011.chunk.js"
+  },
+  {
+    "revision": "18d5033931d5b9b5c7e7",
+    "url": "/SocialNetwork/static/js/runtime-main.9bf3549e.js"
+  },
+  {
+    "revision": "c863daf947cd38a8aaf09b192dd4c8dc",
+    "url": "/SocialNetwork/static/media/preloader2.c863daf9.gif"
+  }
+]);
